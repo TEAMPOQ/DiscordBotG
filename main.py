902ca76932bab54046434430d2989e9b971ca9b3
TEAMPOQ <43634614+TEAMPOQ@users.noreply.github.com>
2023-04-22 22:56:22 -0700

    bug fixes / new commands

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ from discord.ext import commands
 from pytube import YouTube
 from keep_alive import keep_alive
 from refresh import Refresh
-from secrets import spotify_user_id, playlist_id
+from secrets import spotify_user_id, playlist_id, refresh_token, access_token
+import spotipy.util as util
 
 message_play = ""
 intents = discord.Intents.all()
@@ -42,8 +43,20 @@ counter = 0
 playlist_total = 0
 msg = ""
 skip_song = False
-sp = spotipy.Spotify()
+playlist_response = ''
+SPOTIFY_USERNAME='31nqczrdhhi2y4o2pnyv53ihfncq'
+SPOTIPY_CLIENT_ID='3c7d7773da494e2db73293f7361348b3'
+SPOTIPY_CLIENT_SECRET='3b72a8bf993642869c4caf9b6af3fb11'
+SPOTIPY_REDIRECT_URI='https://github.com/TEAMPOQ/DiscordBotG'
 
+playlist_ID = ''
+track_id = ''
+
+sp = spotipy.Spotify(auth=access_token)
+scope = 'playlist-modify-public'
+
+token = spotipy.SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope=scope, username=SPOTIFY_USERNAME)
+spotifyObject = spotipy.Spotify(auth_manager=token)
 
 class SaveSongs:
     def _init_(self):
@@ -59,109 +72,8 @@ class SaveSongs:
         song_to_search = input("Enter Spotify Playlist ID >> ")
         self.song_to_search = "6Rz9QFcQCvqaILMv7y95W2"#song_to_search
 
-    def find_songs(self):
-        # Loop through playlist tracks, add them to list
-        tracks = [" "]
-        print("finding songs in playlist")
-        query = "https://api.spotify.com/v1/playlists/{}/tracks".format(playlist_id)
-        response = requests.get(query,
-                                headers={"Content-Type": "application/json", "Authorization": "Bearer {}".format(self.spotify_token), "Host": "api.spotify.com"})
-
-
-        response_json = response.json()
-
-        print(response)
-
-        for i in response_json["items"]:               #for loop for amount of items in response
-            tracks += (i["track"]["uri"] + ",")        #stroes our tracks into a list
-        self.tracks = tracks[:-1]                      #gets rid of last comma
-
-    def search_playlist(self, id):
-        global songs
-        global artists
-        global playlist_total
-
-        # query search
-        query = "https://api.spotify.com/v1/playlists/{}?limit=25".format(id).replace(" ", "")
-        print(query)
-        response = requests.get(query,
-                                headers={"Content-Type": "application/json", "Authorization": "Bearer {}".format(self.spotify_token), "Host": "api.spotify.com"})
-
-        response_json = response.json()
-        playlist_total = response_json["tracks"]["total"]
-        if playlist_total > 25:
-            playlist_total = 25
-
-        #print(response_json)
-        for x in range(playlist_total):
-            songs.append(response_json["tracks"]["items"][x]["track"]["name"])
-            artists.append(response_json["tracks"]["items"][x]["track"]["album"]["artists"][0]["name"])
-
-
-        print(songs)
-        print(artists)
-
-
-
-    def create_playlist(self):
-        #create a new playlist
-        today = date.today()
-        todayFormatted = today.strftime("%d/%n/%Y")
-        query = "http://api.spotify.com/v1/users/{}/playlists".format(spotify_user_id)# the {} inserts the .format(id)
-
-        request_body = json.dumps({"name": todayFormatted + " Playlist", "description": "Playlist updated with timestamp", "PUBLIC": False})
-
-        response = requests.post(query, data=request_body, headers={"Content-Type": "application/json", "Authorization": "Bearer {}".format(self.spotify_token)})
-
-        response_json = response.json()
-        print(response)
-
-    def search_song(self,msg):
-        global message_play
-
-        self.song_to_search = msg  # song_to_search
-        query = "http://api.spotify.com/v1/search/?type=track&q={}&include_external=audio&limit=4&market=US".format(self.song_to_search)
-        print(query)
-
-
-        global track_name
-        track_name = ["","","","",""]
-        artist_name = ["","","","",""]
-
-        response = requests.get(query, headers={"Content-Type": "application/json", "Authorization":"Bearer {}".format(self.spotify_token)})
-
-        response_json = response.json()
-
-        if response_json["tracks"]["total"] <= 0:                                               # check for zero songs in list
-            print("Playlist is to short")
-
-        track_name[0] = response_json["tracks"]["items"][0]["name"]                             # song name
-        artist_name[0] = response_json["tracks"]["items"][0]["album"]["artists"][0]["name"]     # artist Name
-
-        #set the urls
-        message_play = "https://www.youtube.com/results?search_query={}+by+{}".format(track_name[0].replace(" ", "+"), artist_name[0].replace(" ", "+"))
-
-
-    def get_track(self):
-        query = "https://api.spotify.com/v1/me/player/play"
-
-        response = requests.put(query, data={"uris": "{}".format(self.alb_uri[0]),
-                                "position_ms":"0"},
-                                headers={"Content-Type": "application/json",
-                                                "Authorization":"Bearer {}".format(self.spotify_token)},
-                                )
-        response_json = response.json()
-        print(response_json)
-
-    ########## REFRESH SPOTIFY TOKEN ###########
+    ############### GET PLAYLIST ###############
     ############################################
-    def call_refresh(self):
-        print("Refreshing token")
-
-        refreshCaller = Refresh()
-
-        self.spotify_token = refreshCaller.refresh()
-
     def get_playlists(self):
         global str_msg_list
         global bot_playlist
@@ -179,8 +91,115 @@ class SaveSongs:
             playlist_response = str(playlist_response)[num+9:]
             list_num += 1
 
+    ############ GET PLAYLIST SONGS ############
+    ############################################
+    def get_playlists_songs(self):
+        global str_msg_list, playlist_response, bot_playlist, playlist_ID
+        song_list = []
+        str_msg_list = "SONGS: "
+        list_num = 0
+        sp.set_auth(self.spotify_token)
+        playlist_songs = spotifyObject.playlist_items(playlist_ID, limit=25, offset=0, market=None)
+
+        num_tracks = spotifyObject.playlist_items(playlist_ID, limit=25, offset=0, market=None, fields='total')
+        trackss = str(num_tracks)[10: -1]
+        #print(playlist_songs)
+        for x in range(int(trackss)):
+            print(playlist_songs['items'][int(list_num)]['track']['name'])
+            str_msg_list += str(int(x+1)) + ". " + playlist_songs['items'][int(list_num)]['track']['name'] + " | "
+            list_num += 1
+
+    ############# SEARCH PLAYLIST ##############
+    ############################################
+    def search_playlist(self, id):
+        global songs
+        global artists
+        global playlist_total
+        global playlist_ID
+
+        # query search
+        query = "https://api.spotify.com/v1/playlists/{}?limit=25".format(playlist_ID).replace(" ", "")
+        response = requests.get(query,
+                                headers={"Content-Type": "application/json", "Authorization": "Bearer {}".format(self.spotify_token), "Host": "api.spotify.com"})
+
+        response_json = response.json()
+        playlist_total = response_json["tracks"]["total"]
+        if playlist_total > 25:
+            playlist_total = 25
+
+        #print(response_json)
+        for x in range(playlist_total):
+            songs.append(response_json["tracks"]["items"][x]["track"]["name"])
+            artists.append(response_json["tracks"]["items"][x]["track"]["album"]["artists"][0]["name"])
+
+    ############# Create Playlist ##############
+    ############################################
+    def create_playlist(self, name):
+        #create a new playlist
+        today = date.today()
+        todayFormatted = today.strftime("%d/%n/%Y")
+
+        #sp.set_auth(access_token)
+        spotifyObject.user_playlist_create(user=str(SPOTIFY_USERNAME), name=str(name), public=True, collaborative=False, description=todayFormatted)
 
 
+    ############# Select Playlist ##############
+    ############################################
+    def select_playlist(self, name):
+        global bot_playlist
+        global playlist_ID
+        a.get_playlists()                                                                                           # INITIALIZE bot_playlist
+        index = 0                                                                                                   # KEEP TRACK OF PLAYLIST INDEX
+        for x in bot_playlist:
+            if str(x).find(str(name)) != -1:                                                                        # FIND IF PLAYLIST EXISTS
+                break
+            index += 1
+        playlist_ID = spotifyObject.user_playlists(user=SPOTIFY_USERNAME, offset=index)['items'][index]['id']       # GRAB PLAYLIST ID
+
+    ############# ADD TO PLAYLIST ##############
+    ############################################
+    def add_playlist(self, name):
+        global playlist_ID
+        global track_id
+        a.search_song(name)
+        print(track_id)
+        spotifyObject.playlist_add_items(playlist_id=playlist_ID, items=[track_id], position=0)
+
+
+    ############### SEARCH SONG ################
+    ############################################
+    def search_song(self,msg):
+        global message_play
+        global track_name
+        global track_id
+
+        self.song_to_search = msg  # song_to_search
+        query = "http://api.spotify.com/v1/search/?type=track&q={}&include_external=audio&limit=4&market=US&limit=1".format(self.song_to_search)
+
+        track_name = ["","","","",""]
+        artist_name = ["","","","",""]
+
+        response = requests.get(query, headers={"Content-Type": "application/json", "Authorization":"Bearer {}".format(self.spotify_token)})
+        response_json = response.json()
+
+        if response_json["tracks"]["total"] <= 0:                                               # check for zero songs in list
+            print("Playlist is to short")
+
+        track_id = response_json["tracks"]["items"][0]["id"]                                    # song id
+        track_name[0] = response_json["tracks"]["items"][0]["name"]                             # song name
+        artist_name[0] = response_json["tracks"]["items"][0]["album"]["artists"][0]["name"]     # artist Name
+
+        #set the urls
+        message_play = "https://www.youtube.com/results?search_query={}+by+{}".format(track_name[0].replace(" ", "+"), artist_name[0].replace(" ", "+"))
+
+    ########## REFRESH SPOTIFY TOKEN ###########
+    ############################################
+    def call_refresh(self):
+        print("Refreshing token")
+
+        refreshCaller = Refresh()
+
+        self.spotify_token = refreshCaller.refresh()
 
 
 @client.event
@@ -211,11 +230,29 @@ async def on_message(ctx):
         a.get_playlists()
         await sendMsg(ctx, str_msg_list)
 
+    ######## LIST ALL SONG IN PLAYLIST #########
+    ############################################
+    if msg.startswith('$p songs'):
+        a.get_playlists_songs()
+        await sendMsg(ctx, str_msg_list)
+
     ########### CREATE A  PLAYLIST #############
     ############################################
     if msg.startswith('$p create'):
-        await create_playlist(ctx, msg[10:])                #send command to create a new playlist
+        a.create_playlist(msg[10:])                #send command to create a new playlist
 
+    ########### SELECT A  PLAYLIST #############
+    ############################################
+    if msg.startswith('$p select'):
+        a.select_playlist(msg[10:])                #send command to create a new playlist
+
+    ############## ADD A PLAYLIST ##############
+    ############################################
+    if msg.startswith('$p add'):
+        if len(playlist_ID) < 1:
+            await sendMsg('SELECT A PLAYLIST BEFORE ADDING A SONG-__-')
+        else:
+            a.add_playlist(msg[7:])
 
     ############# PLAY A  PLAYLIST #############
     ############################################
@@ -265,17 +302,13 @@ async def on_message(ctx):
 ########################################################################
 ########################################################################
 
+
+
+############### SEND A MSG FUNCTION #############
+#################################################
 async def sendMsg(ctx, str_msg):
     music_channel = client.get_channel(965814271063781396)
     await music_channel.send(str_msg)
-
-
-async def create_playlist(ctx, name):
-    sp.set_auth(a.spotify_token)
-    response = requests.get('https://api.spotify.com/v1/users/{}/playlists?name={}'.format(sp.set_auth(a.spotify_token),name))
-    print(response)
-    #sp.user_playlist_create(spotify_user_id, name, public=True, collaborative=False, description='')
-
 
 ################ SPAM USER FUNCTION #############
 #################################################
@@ -296,32 +329,49 @@ async def spamUser(ctx, username):
 @client.event
 async def playlistplay(ctx):
     global playlist_total
+    global playlist_ID
     global counter
     global msg
     global skip_song
+    global video_length
 
     if ctx.author == client.user:                                       # checks to see if the message was sent by a bot
         return
     channel = ctx.author.voice.channel
-
+    voice = ctx.channel.guild.voice_client
+    video_length.clear()
     # search playlist function call with spotify api
     a.search_playlist(msg[9:32].replace(" ", ""))
-    try:
-        for counter in range(playlist_total):
-            a.search_song(songs[counter] + " " + artists[counter])      # search for song
-            await getYoutubeUrls()                                      # generate youtube url
-            await download(ctx)                                         # download song
-            await play(ctx)                                             # call play function
-            test = asyncio.get_running_loop()                           # wait for song to finish
-            end = test.time() + video_length[int(counter)][0]           # calculate the end of the current song
+    for counter in range(playlist_total):
+        a.search_song(songs[counter] + " " + artists[counter])      # search for song
 
-            while True:                                                 # while song is playing do
-                print(datetime.datetime.now())                          # print time stamp
-                if (test.time() + 1.0) >= end or skip_song is True:     # loop break condition
-                    skip_song = False                                   # reset skip condition
-                    break
-                await asyncio.sleep(1)                                  # timeout
-    except:
+        await getYoutubeUrls()                                      # generate youtube url
+        try:
+            await download(ctx)
+            # play song function
+        except:
+            print('error with download')
+        try:
+            if voice is None:
+                voice = await channel.connect()
+            elif voice.channel != channel:
+                voice.move_to(channel)
+
+            source = FFmpegPCMAudio("song.mp3")
+            player = voice.play(source)
+        except:
+            print("error playing song")
+
+        test = asyncio.get_running_loop()                           # wait for song to finish
+        end = test.time() + video_length                            # calculate the end of the current song
+
+        while True:                                                 # while song is playing do
+            print(datetime.datetime.now())                          # print time stamp
+            if (test.time() + 1.0) >= end or skip_song is True:     # loop break condition
+                skip_song = False                                   # reset skip condition
+                break
+            await asyncio.sleep(1)                                  # timeout
+
         print("playlist error")                                         # catch any execution errors with playlist
     counter = 0                                                         # reset counter
     discord.player.VoiceClient.stop(ctx)                                # force stop voice client
@@ -452,6 +502,7 @@ async def stop(ctx):
 async def getYoutubeUrls():
     global message_play
     global watch_link
+    global video_length
     video_ids = []
 
     #html = urllib.request.urlopen(message_play)
@@ -460,6 +511,8 @@ async def getYoutubeUrls():
     video_ids = re.findall(r"watch\?v=(\S{11})", str(response))
 
     watch_link = "https://www.youtube.com/watch?v=" + video_ids[0]
+    vid = pytube.YouTube(watch_link)
+    video_length = vid.length
     print(watch_link)
 
 ############# RESET VARIABLES FUNCTION ##############
