7a8db51833a770cabd811ad27a554022611fca3f
TEAMPOQ <43634614+TEAMPOQ@users.noreply.github.com>
2023-07-01 01:13:03 -0700

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,20 +1,9 @@
 #spotify_user_id = "bcfc853b19be4d86ae2056410e736c33"
 #refresh_token = "AQAnKGNSXLg9r0N39EKtG5PEFtE45M8q69Y2f6PwRn6k2KJ8DRZjVqQMObqxTv2TeBqCmSXpy9E79HSl4S7Pz9RjwD7FMmuePJgxezZgb-3YXAlU9tA0OEgyrOvyDHzLBRY"
 #base_64 = "YmNmYzg1M2IxOWJlNGQ4NmFlMjA1NjQxMGU3MzZjMzM6YWNkYzMxMzhlODU3NGFkZThjOWZiNzI1M2FlNzZhY2Y="
-playlist_id = "37i9dQZEVXbMDoHDwVN2tF"
-refresh_token = 'AQClSIE3F5jtZWuyd4STTR3WJH4y9t6e52CfNI2fH89CmYrzfNx4qd5t9TN2K-K81Z_ndX_Y0WM8SoXUCxXMZkSWAqt10u35XzXzshU6L9MzMeGXzbp7qF9BKSj90TNkqwk'
-access_token = 'BQD6S7A3XE1bm8WMdA_9GNptU6FotdV6_eQt7nrfGu14v-BH8trMbbd0dzC44spkBaIJeAeVOE_a-7wdG8dI7M82uT4MHS_cLLGWa8VIH8ZXUy_ZvZ7cira7HGVnFyY6cMYri5tlxlHiXlPUK48c4vAjm3GV6_-2sf-il8XNy4FqDj-Xm3nwOcBRnxz7ljzLwWrRBAg'
-spotify_user_id = '3b72a8bf993642869c4caf9b6af3fb11'
-base_64 = 'M2M3ZDc3NzNkYTQ5NGUyZGI3MzI5M2Y3MzYxMzQ4YjM6M2I3MmE4YmY5OTM2NDI4NjljNGNhZjliNmFmM2ZiMTE='
+playlist_id = ""
+refresh_token = ''
+access_token = ''
+spotify_user_id = ''
+base_64 = ''
 
-#BQDuJT-5XDtePckbg_Zl_478_uInYlSSgyVvi_MLSbjph_EXwSmluLfvOur4pmYrEkbn9Xsw0gVNFPLhzTEVaBEEOmNSo5Sen-oFp0OB5MHYRgThSThnWcr7SmZ2wZmF0J7PZm1rKHg5JcFAYe9xLsGWtxxP9PcbbUH6CsA47XASv0tbRAbpGdJ27xuwyz7OE43eAkU
-#AQClSIE3F5jtZWuyd4STTR3WJH4y9t6e52CfNI2fH89CmYrzfNx4qd5t9TN2K-K81Z_ndX_Y0WM8SoXUCxXMZkSWAqt10u35XzXzshU6L9MzMeGXzbp7qF9BKSj90TNkqwk
-#Client ID bcfc853b19be4d86ae2056410e736c33
-#Client Secret acdc3138e8574ade8c9fb7253ae76acf
-
-#bcfc853b19be4d86ae2056410e736c33:acdc3138e8574ade8c9fb7253ae76acf
-#https://accounts.spotify.com/authorize?client_id=bcfc853b19be4d86ae2056410e736c33&response_type=code&redirect_uri=https%3A%2F%2Flocoaascasc.githubs.io&scope=playlist-modify-public%20playlist-modify-private
-'''
-https://accounts.spotify.com/authorize?response_type=code&client_id=3c7d7773da494e2db73293f7361348b3:3b72a8bf993642869c4caf9b6af3fb11&redirect_uri=https://github.com/TEAMPOQ/DiscordBotG
-#curl -H "Authorization: Basic M2M3ZDc3NzNkYTQ5NGUyZGI3MzI5M2Y3MzYxMzQ4YjM6M2I3MmE4YmY5OTM2NDI4NjljNGNhZjliNmFmM2ZiMTE=" -d grant_type=authorization_code -d code=AQDv8-cZPT4wZ1HaevnLOMumb1U0paMS0gTe0YEPF3o0ZFMP0Uz_Z2N2agzxkU65mbuPczK_mfkYpQ8ECkK733Wf8sA72FE31_jfwbFFin8lD2BWoLHNBNHeeaKVFithYN07rbuICa2SpsJdaIXjnSSrXWtUH9wmBqTeDuUBqJNop3JawoZ2sxhp -d redirect_uri=https://github.com/TEAMPOQ/DiscordBotG https://accounts.spotify.com/api/token
-'''
